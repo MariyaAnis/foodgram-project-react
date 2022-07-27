bc47af42f787f82d3352be9dab866707410975a4
mary <anisimovatriz@gmail.com>
2022-07-27 14:36:32 +0300

    Fix tag

--- backend/api/views.py
+++ backend/api/views.py
@@ -26,10 +26,10 @@ from .serializers import (IngredientSerializer, IngredientWeightSerializer,
 class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
-    permission_classes = (IsAuthorOrReadOnlyPermission,)
+    # permission_classes = (IsAuthorOrReadOnlyPermission,)
     filter_backends = [DjangoFilterBackend]
     filterset_class = RecipeFilter
-    pagination_class = LimitOffsetPagination
+    # pagination_class = LimitOffsetPagination
 
     def get_serializer_class(self):
         if self.request.method == 'POST' or self.request.method == 'PATCH':
