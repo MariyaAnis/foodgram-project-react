372a0fb093e8f21e6d1886db0dc76d6e02b152f0
mary <anisimovatriz@gmail.com>
2022-07-28 13:42:25 +0300

    Fix filters

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,7 +3,7 @@ from django.http import FileResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from djoser.views import UserViewSet as DjUserViewSet
-from rest_framework import filters, status, viewsets
+from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
