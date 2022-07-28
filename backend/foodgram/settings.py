ce423e789b008c5b908976d8eb851382a50a3475
mary <anisimovatriz@gmail.com>
2022-07-28 12:49:55 +0300

    fix bags

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -117,7 +117,7 @@ REST_FRAMEWORK = {
         'django_filters.rest_framework.DjangoFilterBackend'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 6
+    'PAGE_SIZE': 6  # nado?
 }
 
 DJOSER = {
