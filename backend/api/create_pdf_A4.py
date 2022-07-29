9ed64d575d6ede74d7a0458c01293c2d4bdbc0da
mary <anisimovatriz@gmail.com>
2022-07-29 14:00:58 +0300

    fix import ttf file

--- backend/api/create_pdf_A4.py
+++ backend/api/create_pdf_A4.py
@@ -12,7 +12,7 @@ def create_pdf(title, ingredients):
     p = canvas.Canvas(buffer, pagesize=A4)
 
     pdfmetrics.registerFont(
-        TTFont('FreeSans', 'FreeSans.ttf'))
+        TTFont('FreeSans', 'FreeSans.ttf'))  # docker
 
     p.setFont('FreeSans', 20)
     y = 810
